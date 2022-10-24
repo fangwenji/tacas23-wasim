44d5c8f2ec3292e4af130e8323b6338bc7eac427
wenjifang <wenjifang@ust.hk>
2022-10-24 11:47:28 +0800

    tacas23-wasim

--- deps/setup.sh
+++ deps/setup.sh
@@ -3,6 +3,8 @@
 
 
 echo "Install Dependencies"
+
+DEPSDIR=$(pwd)
 #1. install pysmt (pip3)
 pip3 install PySMT-0.9.5.tar.gz
 
@@ -12,23 +14,23 @@ pip3 install z3_solver-4.8.15.0-py2.py3-none-manylinux1_x86_64.whl
 #3. install boolector (pip3)
 pip3 install PyBoolector-3.2.2.20221010.1-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl
 
-#4. install cvc5 (copy)
-cp cvc5-Linux /home/tacas23/
+#4. cvc5-Linux
+chmod chmod 755 cvc5-Linux
 
-echo "All Dependencies Are Installed Successfully!"
 
-#5. copy source code
-echo "Copy wasim to New Path"
-cp -rf /home/tacas23/tacas23-wasim/wasim /home/tacas23/
 
-cd /home/tacas23/wasim/
+cd $(DEPSDIR)/../
+cp -rf wasim ~/
+export WASIMROOT=~/wasim
+echo "export WASIMROOT=~/wasim" >> ~/.bashrc
+cd ~/
+chmod -R 755 wasim
 
-mkdir output
-mkdir log
-cd log
-mkdir log_demo
-mkdir log_simulation
-mkdir log_proof
+echo
+echo "---------------------------"
+echo "  Installation Completed.  "
+echo "---------------------------"
 
+cd $(DEPSDIR)
 
 
